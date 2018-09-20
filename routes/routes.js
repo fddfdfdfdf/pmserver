35a732962e5278127fb30354206d180bfa5dccd3
yangdupeng@cftig.com <yangdupeng@cftig.com>
2018-09-20 10:51:20 +0800

    node ejs update

--- routes/routes.js
+++ routes/routes.js
@@ -9,9 +9,19 @@ router.get('/', function (req, res) {
      // requstData._get(function (data){
      //   res.send(data)
      // })
-    requstData._post(function (data){
-      res.send(data)
-    })
+    // requstData._post(function (data){
+    //   res.send(data)
+    // })
+    var users = [
+        {name: 'tobi', email: 'tobi@learnboost.com'},
+        {name: 'loki', email: 'loki@learnboost.com'},
+        {name: 'jane', email: 'jane@learnboost.com'}
+    ];
+    res.render('index', {
+        users:users,
+        title: "EJS example",
+        header: "Some users"
+    });
     // res.sendFile(path.resolve(__dirname+"/../view/index.html"));
 })
 router.get('/ff', function (req, res) {
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/routes.js');
+const path = require('path');
 
 
+
+
+
+//注册ejs模板为html页。简单的讲，就是原来以.ejs为后缀的模板页，现在的后缀名可以//是.html了
+app.engine('.html', require('ejs').__express);
+//设置视图模板的默认后缀名为.html,避免了每次res.Render("xx.html")的尴尬
+app.set('view engine', 'html');
+//设置模板文件文件夹,__dirname为全局变量,表示网站根目录
+app.set('views', path.join(__dirname, './views'));
+//设置静态变量
 app.use(express.static("./public"))
 
 
